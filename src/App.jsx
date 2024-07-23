b2deb81905c3345fb7cb655a20a2bce200101059
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-07-23 13:16:40 +0330

    Create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 // import "./App.css";
 
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <Navbar />
       <Outlet />
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">00 Home</Link>
+        </li>
+        <li>
+          <Link to="/destination">01 Destination</Link>
+        </li>
+        <li>
+          <Link to="/crew">02 Crew</Link>
+        </li>
+        <li>
+          <Link to="/technology">03 Technology</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
