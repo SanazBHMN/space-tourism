adc50f1a71fe85eea02dc9291c0244a21038bb66
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-07-25 15:48:00 +0330

    Programatically set background images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 // static imports
 import "./App.scss";
 
 function App() {
+  const location = useLocation();
+  const url = location.pathname.endsWith("/")
+    ? "home"
+    : location.pathname.startsWith("/destination")
+    ? "destination"
+    : location.pathname.startsWith("/crew")
+    ? "crew"
+    : "technology";
+
   return (
-    <div className="App">
+    <div className={`App bg bgImage-${url}`}>
       <Navbar />
       <Outlet />
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,24 @@
+// static imports
+import "./home-page.scss";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="homepage-container">
+      <div className="content">
+        <h1 className="content__header">So, you want to travel to Space</h1>
+        <p className="content__paragraph">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience! ExploreSo, you want to travel to Space Let’s face it; if
+          you want to go to space, you might as well genuinely go to outer space
+          and not hover kind of on the edge of it. Well sit back, and relax
+          because we’ll give you a truly out of this world experience!
+        </p>
+        <div className="cta">Explore</div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
