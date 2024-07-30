afbccd31cde149171f16a1f0909eccd6ac03930b
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-07-30 12:47:08 +0330

    Create useSize hook

---
+++ src/hooks/useSize.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const useSize = () => {
+  const [windowSize, setWindowWSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+
+  useEffect(() => {
+    const windowSizeHandler = () => {
+      setWindowWSize([window.innerWidth, window.innerHeight]);
+    };
+    window.addEventListener("resize", windowSizeHandler);
+    return () => {
+      window.removeEventListener("resize", windowSizeHandler);
+    };
+  }, []);
+
+  return windowSize;
+};
+
+export default useSize;
